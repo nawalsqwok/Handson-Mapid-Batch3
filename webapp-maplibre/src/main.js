07e2964471b5f45b728ef248c6fb60d49f6b3aff
Nawal Syafiq Farihan <Nawalsyafiq31@gmail.com>
2026-07-12 11:25:35 +0700

    cretae initial webmap with maplibre

---
+++ webapp-maplibre/src/main.js
@@ -0,0 +1,11 @@
+import {Map} from 'maplibre-gl';
+
+const mapElement = document.createElement('div');
+mapElement.id = 'map';
+mapElement.style.height = "300px";
+document.body.appendChild(mapElement);
+
+const map = new Map({
+    container: 'map',
+    style: 'https://demotiles.maplibre.org/globe.json'
+})
