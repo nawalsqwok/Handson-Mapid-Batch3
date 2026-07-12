ffcb45797ff80975afc54294068bc273ab496bad
Nawal Syafiq Farihan <Nawalsyafiq31@gmail.com>
2026-07-12 21:53:27 +0700

    add new sample gejson layer

--- webapp-maplibre/src/main.js
+++ webapp-maplibre/src/main.js
@@ -7,5 +7,45 @@ document.body.appendChild(mapElement);
 
 const map = new Map({
     container: 'map',
-    style: 'https://demotiles.maplibre.org/globe.json'
+    style: 'https://demotiles.maplibre.org/globe.json',
+    center: [106.89, -6.19],
+    zoom: 5,
+});
+
+const data = {
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {
+        "Name": "Jakarta"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          106.8411325,
+          -6.2728788
+        ]
+      }
+    }
+  ]
+}
+
+map.on("load",() => {
+   map.addSource('kota', {
+    type: 'geojson',
+    data: data
 })
+map.addLayer({
+    id: "titik-kota",
+    type: "circle",
+    source: "kota",
+    paint: {
+        "circle-radius": 10,
+        "circle-color": "yellow",
+        "circle-stroke-width": 1,
+        "circle-stroke-color": "black"
+    }
+    })    
+})
+
