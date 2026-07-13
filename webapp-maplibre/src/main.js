8f15c03134806ecd044ce502c0f424cc2ce9f936
Nawal Syafiq Farihan <Nawalsyafiq31@gmail.com>
2026-07-13 11:38:03 +0700

    add raster into webmap

--- webapp-maplibre/src/main.js
+++ webapp-maplibre/src/main.js
@@ -33,12 +33,16 @@ const map = new Map({
 //   ]
 // }
 
+
+// ----- layer vektor titik
+
 map.on("load",() => {
    map.addSource('kota', {
     type: 'geojson',
     data: naturalEarthData
 })
 
+// ------ layer vector polygon
 map.addSource('pulau', {
         type: "geojson",
         data: areaData,
@@ -53,7 +57,7 @@ map.addSource('pulau', {
             "fill-outline-color": "black"
         }
     })
-    
+
 map.addLayer({
     id: "titik-kota",
     type: "circle",
@@ -64,6 +68,24 @@ map.addLayer({
         "circle-stroke-width": 1,
         "circle-stroke-color": "black"
     }
+    })
+
+    // ----- layer raster
+    map.addSource("gedungSate", {
+        type: "image",
+        url: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4c/Gedung_Sate_Oktober_2024_-_Rahmatdenas.jpg/1280px-Gedung_Sate_Oktober_2024_-_Rahmatdenas.jpg",
+        coordinates: [
+            [107.44, -5.691], // top-left
+            [110.16, -6.32], // top-right
+            [111.11, -9.34], // bottom-right
+            [106.38, -9.14] // bottom-left
+        ] 
+    })
+
+    map.addLayer({
+        id: "gedungsate-image",
+        type: "raster",
+        source: "gedungSate",
     })
     
 })
