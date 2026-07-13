e657a4b7cc8793106aa3f2cbb1b6eb81374214c6
Nawal Syafiq Farihan <Nawalsyafiq31@gmail.com>
2026-07-13 10:28:21 +0700

    create gejsnon file to webmap

--- webapp-maplibre/src/main.js
+++ webapp-maplibre/src/main.js
@@ -1,4 +1,5 @@
 import {Map} from 'maplibre-gl';
+import naturalEarthData from "./data/ne.geojson?url";
 
 const mapElement = document.createElement('div');
 mapElement.id = 'map';
@@ -12,36 +13,36 @@ const map = new Map({
     zoom: 5,
 });
 
-const data = {
-  "type": "FeatureCollection",
-  "features": [
-    {
-      "type": "Feature",
-      "properties": {
-        "Name": "Jakarta"
-      },
-      "geometry": {
-        "type": "Point",
-        "coordinates": [
-          106.8411325,
-          -6.2728788
-        ]
-      }
-    }
-  ]
-}
+// const data = {
+//   "type": "FeatureCollection",
+//   "features": [
+//     {
+//       "type": "Feature",
+//       "properties": {
+//         "Name": "Jakarta"
+//       },
+//       "geometry": {
+//         "type": "Point",
+//         "coordinates": [
+//           106.8411325,
+//           -6.2728788
+//         ]
+//       }
+//     }
+//   ]
+// }
 
 map.on("load",() => {
    map.addSource('kota', {
     type: 'geojson',
-    data: data
+    data: naturalEarthData
 })
 map.addLayer({
     id: "titik-kota",
     type: "circle",
     source: "kota",
     paint: {
-        "circle-radius": 10,
+        "circle-radius": 7,
         "circle-color": "yellow",
         "circle-stroke-width": 1,
         "circle-stroke-color": "black"
