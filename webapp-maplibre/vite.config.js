18f7a702b685b7766e45713f79eef2bdb0d0e11b
Nawal Syafiq Farihan <Nawalsyafiq31@gmail.com>
2026-07-27 14:40:44 +0700

    Fix asia folder name

--- webapp-maplibre/vite.config.js
+++ webapp-maplibre/vite.config.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { defineConfig } from "vite";
 import { fileURLToPath, URL } from "node:url";
 
@@ -11,3 +12,19 @@ export default defineConfig({
     },
   },
 });
+=======
+import { defineConfig } from "vite";
+import { fileURLToPath, URL } from "node:url";
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: fileURLToPath(new URL("index.html", import.meta.url)),
+        indonesia: fileURLToPath(new URL("asia/Indonesia.html", import.meta.url)),
+      },
+    },
+  },
+});
+
+>>>>>>> 73a125a (Fix asia folder name)
